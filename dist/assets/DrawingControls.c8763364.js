cbaa1dc5e7c55cd4efd8547546d70b9c6913f26b
johnloy <loy.john@gmail.com>
2023-01-29 14:47:22 -0500

    Revert to rendering to dist dir

---
+++ dist/assets/DrawingControls.c8763364.js
@@ -0,0 +1,1 @@
+import"./DrawingControls.vue_vue_type_script_setup_true_lang.0d39f7d0.js";import{_ as t}from"./DrawingControls.vue_vue_type_script_setup_true_lang.0d39f7d0.js";import"./index.254a4eba.js";export{t as default};
